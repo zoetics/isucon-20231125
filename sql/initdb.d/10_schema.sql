11ad625f94ffdf8db55d8469c44b0d4931bedc1c
IkezoeMakoto <150283t@gmail.com>
2023-12-03 19:47:10 +0900

    fix: isudnsの初期化を直した

--- sql/initdb.d/10_schema.sql
+++ sql/initdb.d/10_schema.sql
@@ -128,24 +128,3 @@ CREATE TABLE `reactions` (
   `created_at` BIGINT NOT NULL,
   INDEX user_id_idx(`user_id`)
 ) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-
-
-USE `isudns`;
-DROP TABLE `records`;
-CREATE TABLE `records` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `domain_id` int DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `type` varchar(10) DEFAULT NULL,
-  `content` varchar(64000) DEFAULT NULL,
-  `ttl` int DEFAULT NULL,
-  `prio` int DEFAULT NULL,
-  `disabled` tinyint(1) DEFAULT '0',
-  `ordername` varchar(255) CHARACTER SET latin1 COLLATE latin1_bin DEFAULT NULL,
-  `auth` tinyint(1) DEFAULT '1',
-  PRIMARY KEY (`id`),
-  KEY `records_name_type` (`name`, `type`),
-  KEY `domain_id` (`domain_id`),
-  KEY `ordername` (`ordername`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-create index records_name_type on records(name, type);
